29698c6f56fb9a24abd202f3de8f12252ceadf49
zls <1460662245@qq.com>
2025-03-12 17:54:12 +0800

    ArticleController test

---
+++ blog/src/main/java/com/ruoxijun/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.ruoxijun;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.ruoxijun.mapper")
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
---
+++ blog/src/main/java/com/ruoxijun/controller/ArticleController.java
@@ -0,0 +1,26 @@
+package com.ruoxijun.controller;
+
+import com.ruoxijun.domain.entity.Article;
+import com.ruoxijun.service.ArticleService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @Resource
+    private ArticleService articleService;
+
+    @GetMapping("/list")
+    public List<Article> list() {
+        List<Article> list = articleService.list();
+        System.out.println(list);
+        return list;
+    }
+
+}
