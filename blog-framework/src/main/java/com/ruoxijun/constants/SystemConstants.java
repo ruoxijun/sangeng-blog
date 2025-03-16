ab1b5ecb5a099a105a5d7b11bbd6eefccd69889e
ruoxijun <1460662245@qq.com>
2025-03-16 17:12:37 +0800

    ✨ feat: add SystemConstants

---
+++ blog-framework/src/main/java/com/ruoxijun/constants/SystemConstants.java
@@ -0,0 +1,20 @@
+package com.ruoxijun.constants;
+
+public class SystemConstants {
+
+    /**
+     * 文章发布状态 1 已发布
+     */
+    public static final int ARTICLE_STATUS_PUBLISH = 1;
+
+    /**
+     * 文章热门列表分页，页数 1
+     */
+    public static final int ARTICLE_HOT_PAGE_CURRENT = 1;
+
+    /**
+     * 文章热门列表分页，数据量 10
+     */
+    public static final int ARTICLE_HOT_PAGE_SIZE = 10;
+
+}
