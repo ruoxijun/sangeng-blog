53e883741d740c433a1242f5e5315c1cffa1faf4
ruoxijun <1460662245@qq.com>
2025-06-16 20:25:13 +0800

    🐛 fix(LinksVo): add @Data

--- blog-framework/src/main/java/com/ruoxijun/domain/vo/LinksVo.java
+++ blog-framework/src/main/java/com/ruoxijun/domain/vo/LinksVo.java
@@ -1,5 +1,12 @@
 package com.ruoxijun.domain.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LinksVo {
     /**
      * 友链 id
