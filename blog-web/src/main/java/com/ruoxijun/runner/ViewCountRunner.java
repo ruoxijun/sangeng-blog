ed1e5b3690c85de49e6cde271f0a03df427f6972
zls <1460662245@qq.com>
2025-09-04 15:27:24 +0800

    ✨ feat: add runner updateViewCount

---
+++ blog-web/src/main/java/com/ruoxijun/runner/ViewCountRunner.java
@@ -0,0 +1,21 @@
+package com.ruoxijun.runner;
+
+import com.ruoxijun.service.ArticleService;
+import com.ruoxijun.utils.RedisCache;
+import jakarta.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ViewCountRunner implements CommandLineRunner {
+
+    @Resource
+    private ArticleService articleService;
+    @Resource
+    private RedisCache redisCache;
+
+    @Override
+    public void run(String... args) throws Exception {
+        articleService.updateArticleViewCountAll();
+    }
+}
