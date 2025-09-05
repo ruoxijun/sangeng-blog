a53da8d6cf851b61ad274ddd19aa6baeee3c2bf5
zls <1460662245@qq.com>
2025-09-05 15:07:58 +0800

    📝 docs: add TagController doc comment

--- blog-admin/src/main/java/com/ruoxijun/controller/TagController.java
+++ blog-admin/src/main/java/com/ruoxijun/controller/TagController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * 标签
+ */
 @RestController
 @RequestMapping("/tag")
 public class TagController {
@@ -17,6 +20,11 @@ public class TagController {
     @Resource
     private TagService tagService;
 
+    /**
+     * 获取标签列表
+     *
+     * @return 标签列表
+     */
     @GetMapping("/list")
     public R<List<Tag>> listTags() {
         return R.ok(tagService.list());
